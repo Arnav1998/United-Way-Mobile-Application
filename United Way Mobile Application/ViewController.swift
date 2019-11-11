9aab3c9ebb95dbf6c83af1a4859526b6e25b4cb9
Arnav Singhania <arnavsinghania98@gmail.com>
2019-11-10 19:54:40 -0800

    Initial Commit

---
+++ United Way Mobile Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  United Way Mobile Application
+//
+//  Created by ARNAV SINGHANIA on 11/10/19.
+//  Copyright © 2019 ARNAV SINGHANIA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
