253669f80c629443e00f9e66dac14f3b6a82a3a3
Arnav Singhania <arnavsinghania98@gmail.com>
2019-11-11 19:18:25 -0800

    Added custom table cells and functionality to display output data to user.

--- United Way Mobile Application/ViewController.swift
+++ United Way Mobile Application/ViewController.swift
@@ -13,21 +13,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var countyPickerView: UIPickerView!
     @IBOutlet weak var ageLabel: UILabel!
     @IBOutlet weak var ageTextField: UITextField!
-    //@IBOutlet weak var outputTextField: UITextView!
+    @IBOutlet weak var tableView: UITableView!
     private var selectedCountyCode:String?
     
-    
     private let countyPickerDataSource = [("Alameda County","94501"),("Alpine County","95646"),("Amador County","95601"),("Butte County","95965"),("Calaveras County","95221"),("Colusa County","95912"),("Contra Costa County","94506"),("Del Norte County","95531"),("El Dorado County","95613"),("Fresno County","93210"),("Glenn County","95913"),("Humboldt County","95501"),("Imperial County","92222"),("Inyo County","92328"),("Kern County","93203"),("Kings County","93202"),("Lake County","95422"),("Lassen County","96009"),("Los Angeles County","90001"),("Madera County","93601"),("Marin County","94901"),("Mariposa County","93623"),("Mendocino County","95410"),("Merced County","93620"),("Modoc County","96006"),("Mono County","93512"),("Monterey County","93426"),("Napa County","94503"),("Nevada County","95713"),("Orange County","92864"),("Placer County","95602"),("Plumas County","95915"),("Riverside County","91752"),("Sacramento County","94203"),("San Benito County","95023"),("San Bernardino County","91701"),("San Diego County","91901"),("San Francisco County","94101"),("San Joaquin County","95201"),("San Luis Obispo County","93401"),("San Mateo County","94002"),("Santa Barbara County","93013"),("Santa Clara County","94022"),("Santa Cruz County","95001"),("Shasta County","96001"),("Sierra County","95910"),("Siskiyou County","95568"),("Solano County","94510"),("Sonoma County","94922"),("Stanislaus County","95307"),("Sutter County","95659"),("Tehama County","96021"),("Trinity County","95527"),("Tulare County","93201"),("Tuolumne County","95305"),("Ventura County","91319"),("Yolo County","95605"),("Yuba County","95692")]
+    private let array = ["first","second","third"]
+    private var ouputArray: [(String,Int?,Int?)] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCountyPickerView()
+        setupTableView()
+        setupTextField()
     }
 
     private func setupCountyPickerView() {
         self.countyPickerView.dataSource = self
         self.countyPickerView.delegate = self
         self.countyPickerView.selectRow(3, inComponent: 0, animated: true)
+    }
+    
+    private func setupTableView() {
+        let nib = UINib(nibName: "TableViewCell", bundle: nil)
+        self.tableView.register(nib, forCellReuseIdentifier: "TableViewCell")
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    private func setupTextField() {
+        self.ageTextField.delegate = self
     }
     
     @IBAction func addPersonButtonPressed(_ sender: UIButton) {
@@ -40,63 +54,43 @@ class ViewController: UIViewController {
     
     
     @IBAction func submitButtonPressed(_ sender: UIButton) {
-        //perform query
-        
         if let countyCode = self.selectedCountyCode {
-            
             let url = URL(string: "https://bing.benefitkitchen.com/api/bing?address=\(countyCode)&persons[0][age]=\(ageTextField.text ?? "0")")!
             //create the session object
             let session = URLSession.shared
-            
             //now create the URLRequest object using the url object
             let request = URLRequest(url: url)
-            
             //create dataTask using the session object to send data to the server
             let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
-                
                 guard error == nil else {
                     return
                 }
-                
                 guard let data = data else {
                     return
                 }
-                
                 do {
-                    //create json object from data
-                    //print(String(data:data, encoding: .utf8)!)
-
                     let expenses = try JSONDecoder().decode(ExpensesModel.self, from: data)
-                    print(expenses)
-                    
-                    
-//                    if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
-//
-//                        //print(json)
-//
-//                        var jsonString = ""
-//
-//                        for (key, value) in json {
-//                            jsonString.append(contentsOf: key)
-//                            jsonString.append("=")
-//                            jsonString.append(String(describing: value))
-//                            jsonString.append(contentsOf: ", ")
-//                        }
-//
-//                        DispatchQueue.main.async {
-//                            //self.outputTextField.text = jsonString
-//                        }
-//
-//                    }
+                    self.ouputArray = []
+                    self.createOutputArray(from: expenses)
+                    DispatchQueue.main.async {
+                         self.tableView.reloadData()
+                    }
                 } catch let error {
                     print(error.localizedDescription)
                 }
             })
-            
             task.resume()
-            
         }
-
+    }
+    
+    private func createOutputArray(from expensesObj: ExpensesModel) {
+        self.ouputArray.append(("Housing:",expensesObj.expenses?.rent,expensesObj.expenses?.annualRent))
+        self.ouputArray.append(("Child Care:",expensesObj.expenses?.childcare,expensesObj.expenses?.annualChildcare))
+        self.ouputArray.append(("Food:",expensesObj.expenses?.food,expensesObj.expenses?.annualFood))
+        self.ouputArray.append(("Helth care:",expensesObj.expenses?.healthcare,expensesObj.expenses?.annualHealthcare))
+        self.ouputArray.append(("Transportation:",expensesObj.expenses?.transportation,expensesObj.expenses?.annualTransportation))
+        self.ouputArray.append(("Miscellaneous:",expensesObj.expenses?.misc,expensesObj.expenses?.annualMisc))
+        self.ouputArray.append(("Taxes:",expensesObj.net_taxes,expensesObj.annualNet_taxes))
     }
     
 }
@@ -126,3 +120,29 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
 }
     
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.ouputArray.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
+        cell.leftLabel.text = self.ouputArray[indexPath.row].0
+        if let centerLabelText =  self.ouputArray[indexPath.row].1 {
+            cell.centerLabel.text = "\(centerLabelText)"
+        }
+        if let rightLabelText = self.ouputArray[indexPath.row].2 {
+            cell.rightLabel.text = "\(rightLabelText)"
+        }
+        return cell
+    }
+
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+}
